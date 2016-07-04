53a4ec0831ff82e9b4d1d4ea4460a3d0591bc89e
houyongsheng45 <houyongsheng45@hotmail.com>
2016-07-04 14:41:48 +0800

    Update xueqiu.py

--- xueqiu.py
+++ xueqiu.py
@@ -8,6 +8,7 @@ headers = {'User-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) 
 def get_attractions(url,date=None):
     page = requests.get(url,headers = headers)
     soup = BeautifulSoup(page.text,'lxml')
+
     zhnames = soup.select('#cube-info > div > div.cube-title.sp-color > div.name')
     symbols = soup.select('#cube-info > div > div.cube-title.sp-color > div.symbol')
     dates = soup.select('#cube-info > div > div.cube-color.sp-color > div.cube-createdate > span.date')
@@ -17,36 +18,20 @@ def get_attractions(url,date=None):
     ranks = soup.select('#cube-info > div > div.cube-ranks.fn-clear > div.cube-rank.rank > p > span')
     yuebaos = soup.select('#cube-info > div > div.cube-ranks.fn-clear > div.cube-baobao.rank > p > span')
 
-    for zhname in zhnames:
-        print (zhname.get_text())
+    zhname = zhnames[0].get_text() if zhnames else ''
+    symbol = symbols[0].get_text() if symbols else ''
+    date = dates[0].get_text() if dates else ''
+    tag = tags[0].get_text() if tags else ''
+    period = periods[0].get_text() if periods else ''
+    year = years[0].get_text() if years else ''
+    rank = ranks[0].get_text() if ranks else ''
+    yuebao = yuebaos[0].get_text() if yuebaos else ''
 
-    for symbol in symbols:
-        print (symbol.get_text())
-
-    for date in dates:
-        print (date.get_text())
-
-    for tag in tags:
-        print (tag.get_text())
-
-    for period in periods:
-        print (period.get_text())
-
-    for year in years:
-        print (year.get_text())
-
-    for rank in ranks:
-        print (rank.get_text())
-
-    for yuebao in yuebaos:
-        print (yuebao.get_text())
-
-
-    with open('xueqiu_a.csv','a') as csvfile:
+    with open('xueqiu_b.csv','a') as csvfile:
         fieldnames = ['组合名称','代码',"时间","标签","净值","年收益","月收益排名","对比余额宝"]
         writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
 #        writer.writeheader()
-        writer.writerow({'组合名称':zhname.get_text(),'代码':symbol.get_text(),'时间':date.get_text(),'标签':tag.get_text(),'净值':period.get_text(),'年收益':year.get_text() + "%",'月收益排名':"跑赢" + rank.get_text() + "组合",'对比余额宝':yuebao.get_text()})
+        writer.writerow({'组合名称':zhname,'代码':symbol,'时间':date,'标签':tag,'净值':period,'年收益':year + "%",'月收益排名':"跑赢" + rank + "组合",'对比余额宝':yuebao})
 
 urls_a = ['https://xueqiu.com/p/ZH000' + str(i) for i in range(101,999,1)]
 urls_b = ['https://xueqiu.com/p/ZH00' + str(i) for i in range(1000,9999,1)]
